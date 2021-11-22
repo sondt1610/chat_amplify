027ff3ec9b4f3db5657968ac919dfd46f74fb962
sondt <sondt1610@gmail.com>
2021-11-22 13:52:51 +0700

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import Amplify from "aws-amplify";
+import awsconfig from "../src/aws-exports";
+Amplify.configure({ ...awsconfig});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+export default MyApp
